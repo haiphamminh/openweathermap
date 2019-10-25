2733fd04f0380bcb4ded6babad160bceeea10b6b
hai_pham_minh <hai.phamminh86@gmail.com>
2019-10-25 15:04:10 +0700

    fetch all cities and spy sql execution

---
+++ data-collector/src/main/java/org/openweathermap/data/collector/model/City.java
@@ -0,0 +1,15 @@
+package org.openweathermap.data.collector.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class City {
+    private String country;
+    private String name;
+}
--- data-collector/src/main/java/org/openweathermap/data/collector/scheduling/DataCollectorScheduler.java
+++ data-collector/src/main/java/org/openweathermap/data/collector/scheduling/DataCollectorScheduler.java
@@ -1,17 +1,49 @@
 package org.openweathermap.data.collector.scheduling;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openweathermap.data.collector.model.City;
 import org.openweathermap.data.collector.service.DataCollectorService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DataCollectorScheduler {
     private final DataCollectorService dataCollectorService;
 
+    private static Set<City> cities;
+
+    static {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            File file = new File(DataCollectorScheduler.class.getResource("/data/current.city.list.json")
+                                                             .getFile());
+            cities = mapper.readValue(file, new TypeReference<>() {});
+        } catch (Exception e) {
+            log.error("Failed to read the list of cities", e);
+        }
+    }
+
     @Scheduled(fixedRate = 1 * 60 * 60 * 1000)
+//    @Scheduled(fixedRate = 2 * 60 * 1000)
     public void collectData() {
-        dataCollectorService.fetchCurrentWeatherDataByCityName("Thanh pho Ho Chi Minh,vn", "metrics", "");
+        if (CollectionUtils.isEmpty(cities)) {
+            return;
+        }
+        cities.forEach(city -> {
+            try {
+                String name = city.getName() + "," + city.getCountry();
+                dataCollectorService.fetchCurrentWeatherDataByCityName(name, "metrics", "");
+            } catch (Exception e) {
+                log.error("Failed to fetch data for " + city, e);
+            }
+        });
     }
 }
